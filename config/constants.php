80c804524781ed8894c36203b60dd2762d0982b3
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-02-14 23:37:24 +0000

    Created database constants.

--- config/constants.php
+++ config/constants.php
@@ -2,3 +2,9 @@
 
 define("ROOT_URL", "http://localhost");
 
+define("DB_HOST", "localhost");
+define("DB_TYPE", "mysql");
+define("DB_NAME", "2014_comp10120_y7");
+define("DB_USER", "root");
+define("DB_PASS", "");
+
