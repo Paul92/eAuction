18badf199db9455fccdfdff0dd9d1f2916279353
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-02-14 19:45:31 +0000

    Added header

---
+++ views/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+  <head>
+    <title> Test </title>
+    <link rel="stylesheet" href="public/css/default.css" />
+  </head>
+<body>
+
+  <div id="header">
+    <a href="index">Index</a>
+    <a href="help">Help</a>
+    <a href="login">Login</a>
+    <hr/>
+  </div>
+
+  <div id="content">
