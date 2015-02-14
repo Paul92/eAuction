c95916334fcbfa019345fcd357c18669b84e774d
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-02-14 21:13:35 +0000

    Generating links in views/header.php using the ROOT_URL constant.

--- views/header.php
+++ views/header.php
@@ -7,9 +7,9 @@
 <body>
 
   <div id="header">
-    <a href="index">Index</a>
-    <a href="help">Help</a>
-    <a href="login">Login</a>
+    <a href="<?php echo ROOT_URL;?>/index">Index</a>
+    <a href="<?php echo ROOT_URL;?>/help">Help</a>
+    <a href="<?php echo ROOT_URL;?>/login">Login</a>
   </div>
 
   <div id="content">
