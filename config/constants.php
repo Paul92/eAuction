050d44594b185954a614f61b626f2c49634281f1
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-02-15 00:40:00 +0000

    Added constants for hash generation

--- config/constants.php
+++ config/constants.php
@@ -8,3 +8,13 @@ define("DB_NAME", "2014_comp10120_y7");
 define("DB_USER", "root");
 define("DB_PASS", "");
 
+define("PBKDF2_HASH_ALGORITHM", "sha256");
+define("PBKDF2_ITERATIONS", 1000);
+define("PBKDF2_SALT_BYTE_SIZE", 24);
+define("PBKDF2_HASH_BYTE_SIZE", 24);
+
+define("HASH_SECTIONS", 4);
+define("HASH_ALGORITHM_INDEX", 0);
+define("HASH_ITERATION_INDEX", 1);
+define("HASH_SALT_INDEX", 2);
+define("HASH_PBKDF2_INDEX", 3)
