0e859bf1053b097df5b84729f692ce34fa17100a
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-02-15 02:02:11 +0000

    Added session lib

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require 'libs/Controller.php';
 require 'libs/View.php';
 require 'libs/Model.php';
 require 'libs/Database.php';
+require 'libs/Session.php';
 
 require 'config/constants.php';
 
---
+++ libs/Session.php
@@ -0,0 +1,19 @@
+<?php
+
+class Session {
+    public static function init() {
+        session_start();
+    }
+
+    public static function destory() {
+        session_destroy();
+    }
+
+    public static function set($key, $value) {
+        $_SESSION[$key] = $value;
+    }
+
+    public static function get($key) {
+        return $_SESSION[$key];
+    }
+}
