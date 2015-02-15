65deb95d4c1358ae0e6043bd312b44ee1d82e118
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-02-15 02:40:25 +0000

    Added checks in views/header.php to generate logout button if the user is logged in

--- views/header.php
+++ views/header.php
@@ -9,8 +9,14 @@
   <div id="header">
     <a href="<?php echo ROOT_URL;?>/index">Index</a>
     <a href="<?php echo ROOT_URL;?>/help">Help</a>
+
+    <?php if (Session::get('loggedIn')): ?>
+    <a href="dashboard/logout">Logout</a>
+    <?php else: ?>
     <a href="<?php echo ROOT_URL;?>/login">Login</a>
     <a href="<?php echo ROOT_URL;?>/register">Register</a>
+    <?php endif ?>
+
   </div>
 
   <div id="content">
