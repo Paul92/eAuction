fb4199db7e9946a61d3957b19b456462a9cab449
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-02-15 00:18:41 +0000

    Added basic login form

--- views/login/index.php
+++ views/login/index.php
@@ -1,3 +1,25 @@
-<?php
-echo 'We are in login';
+<h1>
+  LOGIN
+</h1>
 
+<form action="login/run" method="post">
+  <table>
+    <tr>
+      <td>
+        <label>Login</label>
+      </td>
+      <td>
+        <input type="text" name="login"/>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <label>Password</label>
+      </td>
+      <td>
+        <input type="password" name="password"/> <br/>
+      </td>
+    </tr>
+  </table>
+  <input type="submit" name="submit"/>
+</form>
