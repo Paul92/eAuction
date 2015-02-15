b96b4ca62ccf1689a5df23a7b2dfc36660038121
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-02-15 01:25:47 +0000

    Include models/HashModel.php in RegisterModel and LoginModel

--- models/LoginModel.php
+++ models/LoginModel.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'models/HashModel.php';
+
 class LoginModel extends HashModel {
 
     public function __construct() {
@@ -8,5 +10,19 @@ class LoginModel extends HashModel {
 
     public function run() {
         
+    }
+
+    private function validate_password($password, $correct_hash)
+    {
+        $params = explode(":", $correct_hash);
+        if(count($params) < HASH_SECTIONS)
+           return false;
+        $pbkdf2 = base64_decode($params[HASH_PBKDF2_INDEX]);
+        return slow_equals($pbkdf2, pbkdf2($params[HASH_ALGORITHM_INDEX],
+                                           $password, $params[HASH_SALT_INDEX],
+                                           (int)$params[HASH_ITERATION_INDEX],
+                                           strlen($pbkdf2), true
+                                          )
+                          );
     }
 }
--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'models/HashModel.php';
+
 class RegisterModel extends HashModel {
 
     public function __construct() {
@@ -8,5 +10,18 @@ class RegisterModel extends HashModel {
 
     public function run() {
         
+    }
+
+    private function create_hash($password)
+    {
+        // format: algorithm:iterations:salt:hash
+        $salt = base64_encode(mcrypt_create_iv(PBKDF2_SALT_BYTE_SIZE, 
+                                               MCRYPT_DEV_URANDOM));
+        return PBKDF2_HASH_ALGORITHM.':'.PBKDF2_ITERATIONS.":".$salt.":".
+               base64_encode(pbkdf2(PBKDF2_HASH_ALGORITHM, $password, $salt,
+                                    PBKDF2_ITERATIONS, PBKDF2_HASH_BYTE_SIZE,
+                                    true
+                                   )
+                            );
     }
 }
