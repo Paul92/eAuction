69bcd3b726b0fd856f1668c323b6274999f14c7f
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-02-25 11:15:38 +0000

    Using bootstrap in register page

--- views/register/index.php
+++ views/register/index.php
@@ -1,25 +1,195 @@
-<h1>
-  REGISTER
-</h1>
+<div class="container">
+  <form class="form-horizontal" method="post" action="<?php echo ROOT_URL;?>/register/run">
+      <h1 class="page-header"> Registration form </h1>
 
-<form action="login/run" method="post">
-  <table>
-    <tr>
-      <td>
-        <label>Login</label>
-      </td>
-      <td>
-        <input type="text" name="login"/>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <label>Password</label>
-      </td>
-      <td>
-        <input type="password" name="password"/> <br/>
-      </td>
-    </tr>
-  </table>
-  <input type="submit" name="submit"/>
-</form>
+
+      <?php if (!empty($errors)): ?>
+      <h2 class="lead"> Please correct the following errors before continuing: </h2>
+      <?php endif ?>
+
+<?php foreach($errors as $error) {
+        echo '<div class="alert alert-danger" role="alert">';
+        echo '<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"> </span>';
+        echo '<a class="close" data-dismiss="alert">×</a>';
+        echo '<span class="sr-only">Error: </span>';
+        echo '<strong> Error: </strong>' . $error;
+        echo '</div>';
+        }
+?>
+
+      <div class="form-group top10">
+        <h4 class="col-xs-offset-1 lead">Personal details</h4>
+      </div>
+
+        <div class="form-group">
+          <div class="top10 col-sm-12 col-md-6">
+            <label for="nickname" class="control-label col-sm-4">Nickname</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control col-sm-8" id="nickname"
+               value="<?php if(isset($formArray['nickname'])) 
+                                echo $formArray['nickname'];?>"
+               name="nickname" placeholder="Nickname" >
+            </div>
+          </div>
+          <div class="top10 col-sm-12 col-md-6">
+            <label for="password" class="control-label col-sm-4">Password</label>
+            <div class="col-sm-8">
+                <input type="password" class="form-control" id="password"
+                 name="password" placeholder="Password"
+                >
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="top10 col-sm-12 col-md-6">
+            <label for="title" class="control-label col-sm-4">Title</label>
+            <div class="col-sm-8">
+              <select class="form-control" id="title" name="title">
+              <option value="Mr." 
+              <?php if (isset($formArray['title']) 
+                        && (empty($formArray['title']) || $formArray['title'] == 'Mr.'))
+                            echo 'selected="selected"';?>>Mr.</option>
+              <option value="Mrs." 
+              <?php if (isset($formArray['title']) && $formArray['title'] == 'Mrs.') 
+                           echo 'selected="selected"';?>>Mrs.</option>
+              </select>
+            </div>
+          </div>
+          <div class="top10 col-sm-12 col-md-6">
+            <label for="surname" class="control-label col-sm-4">Surname</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" id="surname" value=
+                "<?php if (isset($formArray['surname']))
+                           echo $formArray['surname'];?>"
+                  name="surname" placeholder="Surname">
+            </div>
+          </div>
+
+          <div class="top10 col-sm-12 col-md-6">
+            <label for="firstName" class="control-label col-sm-4">First name</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" id="firstName" value=
+                "<?php if (isset($formArray['firstName']))
+                           echo $formArray['firstName'];?>"
+                  name="firstName" placeholder="First Name">
+            </div>
+          </div>
+          <div class="top10 col-sm-12 col-md-6">
+            <label for="middleName" class="control-label col-sm-4">Middle name</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" id="middleName" value=
+                "<?php if (isset($formArray['middleName']))
+                           echo $formArray['middleName'];?>"
+                  name="middleName" placeholder="Middle Name">
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="top10 col-sm-12 col-md-6">
+            <label for="email" class="control-label col-sm-4">E-mail</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" id="email" value=
+                "<?php if (isset($formArray['email']))
+                           echo $formArray['email'];?>"
+                  name="email" placeholder="E-mail">
+            </div>
+          </div>
+          <div class="top10 col-sm-12 col-md-6">
+            <label for="phone" class="control-label col-sm-4">Phone number</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" id="phone" value=
+                "<?php if (isset($formArray['phone']))
+                            echo $formArray['phone'];?>"
+                  name="phone" placeholder="Phone number">
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group top10">
+            <h4 class="col-xs-offset-1 lead">Address details</h4>
+        </div>
+
+        <div class="form-group">
+          <div class="top10 col-sm-12 col-md-6">
+            <label for="city" class="control-label col-sm-4">City</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" id="city" value=
+                "<?php if (isset($formArray['city']))
+                           echo $formArray['city'];?>"
+                  name="city" placeholder="City">
+            </div>
+          </div>
+          <div class="top10 col-sm-12 col-md-6">
+            <label for="country" class="control-label col-sm-4">Country</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" id="country" value=
+                "<?php if (isset($formArray['country']))
+                           echo $formArray['country'];?>"
+                  name="country" placeholder="Country">
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="top10 col-sm-12 col-md-12">
+            <label for="addressLine1" class="control-label col-sm-4 col-md-2">Address Line 1</label>
+            <div class="col-sm-8 col-md-10">
+                <input type="text" class="form-control" id="addressLine1" value=
+                "<?php if (isset($formArray['addressLine1']))
+                           echo $formArray['addressLine1'];?>"
+                  name="addressLine1" placeholder="Address Line 1">
+            </div>
+          </div>
+          <div class="top10 col-sm-12">
+            <label for="addressLine2" class="control-label col-md-2 col-sm-4">Address Line 2</label>
+            <div class="col-sm-8 col-md-10">
+                <input type="text" class="form-control" id="addressLine2" value=
+                "<?php if (isset($formArray['addressLine2']))
+                           echo $formArray['addressLine2'];?>"
+                  name="addressLine2" placeholder="Address Line 2">
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="top10 col-sm-12 col-md-6">
+            <label for="county" class="control-label col-sm-4">County</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" id="county" value=
+                "<?php if (isset($formArray['county']))
+                           echo $formArray['county'];?>"
+                  name="county" placeholder="County">
+            </div>
+          </div>
+          <div class="top10 col-sm-12 col-md-6">
+            <label for="postCode" class="control-label col-sm-4">Post code</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" value=
+                "<?php if (isset($formArray['postCode']))
+                           echo $formArray['postCode'];?>"
+                   id="postCode" name="postCode" placeholder="Post code">
+            </div>
+          </div>
+        </div>
+
+
+          <div class="form-group">
+            <div class="top10">
+              <div class="col-xs-offset-1 col-sm-offset-4 col-sm-6 col-md-offset-2 
+                      col-xs-offset-1 col-xs-8 col-md-8 col-lg-offset-3 col-lg-6">
+                <div class="checkbox">
+                  <input type="checkbox" name="TC" value="accepted"
+                    <?php if(isset($formArray['TC'])) echo 'checked';?>>
+                        I acknowledge I have read and fully understood the 
+                        <a href="TC">Terms and Conditions</a>.
+                </div>
+              </div>
+              <div class="col-xs-offset-0 col-sm-offset-0 col-xs-2">
+                  <button type="submit" class="top10 btn col-sm-8 btn-primary">Register</button>
+              </div>
+            </div>
+          </div>
+    </form>
+</div>
