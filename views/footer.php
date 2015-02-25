a970af6b5188769d01f0045f8449e2716959f7dd
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-02-25 11:14:40 +0000

    Using bootstrap in footer

--- views/footer.php
+++ views/footer.php
@@ -1,8 +1,14 @@
   </div>
-
-  <div id="footer">
-    FOOTER <br/>
+  <div id="footer" class="navbar navbar-default navbar-bottom bot">
+    <div class="col-xs-offset-4 container">
+    <ul class="nav navbar-nav">
+      <li><a><span class="glyphicon glyphicon-copyright-mark"></span> Copyright</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="TC">Terms & Conditions</a></li>
+      <li><a href="help">Help</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+    </div>
   </div>
-
 </body>
 </html>
