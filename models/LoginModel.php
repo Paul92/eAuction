97c4ee04cec88f6cd5b11affe5950315a14a7e74
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-02-25 11:13:40 +0000

    Updated login model to add input checking and db access

--- models/LoginModel.php
+++ models/LoginModel.php
@@ -4,23 +4,50 @@ require 'models/HashModel.php';
 
 class LoginModel extends HashModel {
 
+    const WRONG_USER_OR_PASS = 'Wrong user of password. Please retry.';
+    const NO_NICKNAME_MSG    = 'Please insert a nickname.';
+    const NO_PASSWORD_MSG    = 'Please insert a password.';
+
     public function __construct() {
         parent::__construct();
     }
 
     public function run() {
-        $login    = $_POST['login'];
-        $password = $_POST['password'];
-        $statement = $this->db->prepare("SELECT * FROM usersTEST WHERE 
-                                      login = :login AND password = :password");
-        $statement->execute(array(':login' => $_POST['login'], 
-                                  ':password' => $_POST['password']));
-        if ($statement->rowCount()) {
-            Session::set('loggedIn', true);
-            header('location: ../index');
-        } else {
-            header('location: ../login/retry');
+        $errors    = array();
+        $formArray = array();
+        if (!isset($_POST['nickname']) || empty($_POST['nickname']))
+            $errors['nickname'] = self::NO_NICKNAME_MSG;
+        else
+            $nickname = $_POST['nickname'];
+
+        if (!isset($_POST['password']) || empty($_POST['password']))
+            $errors['password'] = self::NO_PASSWORD_MSG;
+        else
+            $password = $_POST['password'];
+
+        if (empty($errors)) {
+            $fetchHashQuery = 'SELECT password FROM users WHERE nickname = :nick';
+            $fetchHashStmt  = $this->db->prepare($fetchHashQuery);
+            $fetchHashStmt->execute(array(':nick' => $nickname));
+            $array = $fetchHashStmt->fetch(PDO::FETCH_ASSOC);
+            if (empty($array)) {
+                $errors[] = self::WRONG_USER_OR_PASS;
+            } else {
+                $hash = $array['password'];
+                if (/*$this->validate_password($password, $hash)*/ $password==$hash) {
+                    Session::set('loggedIn', true);
+                    header('location: ../index');
+                } else {
+                    $errors[] = self::WRONG_USER_OR_PASS;
+                }
+            }
         }
+        if (empty($errors))
+            return(array('errors' => $errors,
+                         'formArray' => array('password' => $password,
+                                              'nickname' => $nickname)));
+        else
+            return(array('errors' => $errors));
     }
 
     private function validate_password($password, $correct_hash)
