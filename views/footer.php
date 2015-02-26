495d949d473d15ed7566d7bccce047eab4118418
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-02-26 22:23:23 +0000

    Fixed footer width

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
   </div>
   <div id="footer" class="navbar navbar-default navbar-bottom bot">
-    <div class="col-xs-offset-4 container">
+    <div class="col-xs-offset-4 col-xs-8 container">
     <ul class="nav navbar-nav">
       <li><a><span class="glyphicon glyphicon-copyright-mark"></span> Copyright</a></li>
       <li><a href="#">About</a></li>
