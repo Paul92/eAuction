4a68a6d2629da35d55c14f88a07d0eac5d4c540d
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-03-15 22:05:28 +0000

    Added config.php

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+$PayPalMode         = 'sandbox'; // sandbox or live
+$PayPalApiUsername  = 'hasman2008ukseller1_api1.hotmail.co.uk'; //PayPal API Username
+$PayPalApiPassword  = 'EDJ2H87ET94MN3SR'; //Paypal API password
+$PayPalApiSignature     = 'AFcWxV21C7fd0v3bYYYRCpSSRl31AMCrGeym5-DJiH4FBdGFjkO4Vude'; //Paypal API Signature
+$PayPalCurrencyCode     = 'GBP'; //Paypal Currency Code
+$PayPalCancelURL    = 'http://localhost'; //Cancel URL if user clicks cancel
+?>
