26f6c019947fa56788c946a0a5618b2e8cd0f46d
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-03-20 12:16:44 +0000

    Added help page

--- views/help/index.php
+++ views/help/index.php
@@ -1,2 +1,32 @@
-<?php
-echo 'We are in help';
+<div class="container">
+<h1 class="page-header">Help</h1>
+<p>
+<ul>
+<li><A href="#engvis">English auction - visible end time</A></li>
+<li><A href="#enghid">English auction - hidden end time</A></li>
+<li><A href="#dutch">Dutch auction</A></li>
+<li><A href="#sealed">Sealed-bid auction</A></li>
+<li><A href="#vickrey">Vickrey auction</A></li>
+<li><A href="#buy">Buy It Now</A></li>
+</ul>
+</p>
+<br>
+<p><h3><A name="engvis">English auction - visible end time</A></h3>
+The most well known auction type. A starting price and auction duration is set by the seller, increasing bids are then placed on the product. The highest bid amount and bidder is visible to everybody. The person with the highest bid at the end of the fixed time period wins the auction and purchases the product at their highest bid price.
+<br>
+<p><h3><A name="enghid">English auction - hidden end time</A></h3>
+A variant on the traditional English auction. The only difference is that the auction ends at a random time in a known 30 minute period. This helps to stop masses of bids occurring right at the end of the auction.
+<br>
+<p><h3><A name="dutch">Dutch auction</A></h3>
+Unlike a conventional auction, the price of the product periodically decreases until a bid is made. The seller sets a starting price, lowest price and auction duration. The product price then decreases automatically until it reaches the minimum price at the end of the auction. A bidder can place a bid at any time and the first person to bid buys the product.
+<br>
+
+<p><h3><A name="sealed">Sealed-bid auction</A></h3>
+Essentially the same as an english auction with visible end time but all bids and bidders are hidden. This makes it much harder for bidders as they must decide how much they value the product. They could potentially be the only bidder and place a bid twice the starting price or be one of a million bidders and be completely outbid.
+<br>
+<p><h3><A name="vickrey">Vickrey auction</A></h3>
+A type of sealed-bid auction where the winner of the auction pays the second highest bid rather than their own highest bid. 
+<br>
+<p><h3><A name="buy">Buy It Now</A></h3>
+Pay a fixed price for the product immediately.</p>
+</div>
