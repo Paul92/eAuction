1ac27b9c68ad2a7e72e12c31f72f8798f4a98cf5
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-03-20 12:16:34 +0000

    Added footer urls

--- views/footer.php
+++ views/footer.php
@@ -3,10 +3,10 @@
     <div class="col-xs-offset-4 col-xs-8 container">
     <ul class="nav navbar-nav">
       <li><a><span class="glyphicon glyphicon-copyright-mark"></span> Copyright</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="TC">Terms & Conditions</a></li>
-      <li><a href="help">Help</a></li>
-      <li><a href="#">Contact</a></li>
+      <li><a href="<?php echo ROOT_URL;?>/about">About</a></li>
+      <li><a href="<?php echo ROOT_URL;?>/TC">Terms & Conditions</a></li>
+      <li><a href="<?php echo ROOT_URL;?>/help">Help</a></li>
+      <li><a href="<?php echo ROOT_URL;?>/contact">Contact</a></li>
     </ul>
     </div>
   </div>
