037c6276e459d14e7a0b770b45fb84dcef2d4495
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-03-20 12:45:29 +0000

    Fixed url

--- public/js/main.js
+++ public/js/main.js
@@ -18,7 +18,7 @@ $(function () {
     $('#fileupload').fileupload({
         // Uncomment the following to send cross-domain cookies:
         //xhrFields: {withCredentials: true},
-        url: 'http://localhost/newAuction/upload',
+        url: 'https://web.cs.manchester.ac.uk/mbax4pg2/eauction/newAuction/upload',
         singleFileUploads: false,
         loadImageMaxFileSize: 50000,
         imageMinWidth: 1000,
