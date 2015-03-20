18a08622f1dbd20dc6eb986f28fc0b9b62089b0c
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-03-20 12:16:58 +0000

    Added about page

---
+++ views/about/index.php
@@ -0,0 +1,8 @@
+<div class="container">
+<h1 class="page-header">About eAuction</h1>
+eAuction is a privately owned, for-profit company set-up by a group of students from The University of Manchester.<br><br>
+eAuction is a marketplace that allows users to offer, sell and buy just about anything.<br><br>
+eAuction does not have possession of anything listed or sold through eAuction, and is not involved in the actual transaction between buyers and sellers.<br><br>
+The contract for the sale is directly between buyer and seller.
+</p>
+</div>
