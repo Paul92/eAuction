78e4acb6e3419b7f7f9009889e4b4dea65d3e2fe
Paul Gafton <paul-adrian.gafton@student.manchester.ac.uk>
2015-03-20 12:18:37 +0000

    Updated openedAuctions view

--- views/dashboard/openedAuctions.php
+++ views/dashboard/openedAuctions.php
@@ -2,6 +2,9 @@
 <h1 class="page-header">Opened Auctions</h1>
 <?php require('views/dashboard/index.php'); ?>
 <div style="margin: 0 auto;" class="row">
+<?php if (empty($auctions)): ?>
+<div class="alert alert-warning top20" role="alert">This user has not opened any auctions yet.</div>
+<?php else: ?>
 <table class="col-xs-12">
   <thead>
     <tr class="row">
@@ -13,21 +16,21 @@
   </thead>
   <tbody>
 <?php
-foreach ($auctions as $auction) {
-  echo '<tr class="row">' . "\n";
-  echo '<td class="col-md-3 col-sm-3 col-xs-4">';
-  echo '<a href="' . ROOT_URL . '/item/item/' . $auction['id'] . '">';
-  echo $auction['name'] . "</a></td>\n";
-  echo '<td class="col-md-3 col-sm-3 col-xs-3">';
-  echo '<a href="' . ROOT_URL . '/index/category/' . $auction['categoryId'];
-  echo '">' . $auction['category'] . "</a></td>\n";
-  echo '<td class="col-md-3 col-sm-3 col-xs-3">' . $auction['endDate'] . "</td>\n";
-  echo '<td class="col-md-3 col-sm-3 col-xs-2">' . $auction['auctionType']. "</td>\n";
-  echo "</tr>\n";
-
-}
+  foreach ($auctions as $auction) {
+    echo '<tr class="row">' . "\n";
+    echo '<td class="col-md-3 col-sm-3 col-xs-4">';
+    echo '<a href="' . ROOT_URL . '/item/item/' . $auction['id'] . '">';
+    echo $auction['name'] . "</a></td>\n";
+    echo '<td class="col-md-3 col-sm-3 col-xs-3">';
+    echo '<a href="' . ROOT_URL . '/index/category/' . $auction['categoryId'];
+    echo '">' . $auction['category'] . "</a></td>\n";
+    echo '<td class="col-md-3 col-sm-3 col-xs-3">' . $auction['endDate'] . "</td>\n";
+    echo '<td class="col-md-3 col-sm-3 col-xs-2">' . $auction['auctionType']. "</td>\n";
+    echo "</tr>\n";
+  }
 ?>
   </tbody>
 </table>
+<?php endif;?>
   
 </div>
